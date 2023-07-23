fb014450364a53fc58024f6cf71017db64ee96c3
Niels-Ole Lambertz <github@niels-ole.com>
2023-07-23 14:24:15 +0200

    more fixes

--- renderer/experimental-renderer-windows.go
+++ renderer/experimental-renderer-windows.go
@@ -3,18 +3,23 @@
 
 package renderer
 
-import "errors"
+import (
+	"errors"
+	"net/http"
+	"os"
+	"time"
+)
 
 func LoadData(path string, maxZ uint32, tempFile *os.File) (*Data, error) {
-	return _, errors.New("not implemented on Windows")
+	return nil, errors.New("not implemented on Windows")
 }
 
 func Mmap(path string) (*[]byte, *os.File, error) {
-	return _, _, errors.New("not implemented on Windows")
+	return nil, nil, errors.New("not implemented on Windows")
 }
 
 func HandleRenderRequest(w http.ResponseWriter, r *http.Request, duration time.Duration, data *Data, maxTreeDepth uint32, mmapData *[]byte) {
-	return _, errors.New("not implemented on Windows")
+	return nil, errors.New("not implemented on Windows")
 }
 
 func Munmap(data *[]byte) error {
