4581a49499c1ee29dbd8c3580844a586fceba6ea
Niels-Ole Lambertz <github@niels-ole.com>
2023-07-23 14:17:11 +0200

    Compiling on windows

---
+++ renderer/experimental-renderer-windows.go
@@ -0,0 +1,18 @@
+//go:build windows
+// +build windows
+
+package renderer
+
+import "errors"
+
+func LoadData(path string, maxZ uint32, tempFile *os.File) (*Data, error) {
+	return _, errors.New("not implemented on Windows")
+}
+
+func Mmap(path string) (*[]byte, *os.File, error) {
+	return _, _, errors.New("not implemented on Windows")
+}
+
+func HandleRenderRequest(w http.ResponseWriter, r *http.Request, duration time.Duration, data *Data, maxTreeDepth uint32, mmapData *[]byte) {
+	return _, errors.New("not implemented on Windows")
+}
